0392e69f1a8b18236391ad419ac286178c5a5a55
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 16:57:58 -0600

    Selecciona elementos por su clase

---
+++ getElementsByClassName.js
@@ -0,0 +1,20 @@
+
+
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+
+const hero = document.getElementsByClassName('hero');
+console.log(hero);
+
+
+// Si las clases existen más de una vez...
+const contenedores = document.getElementsByClassName('contenedor');
+console.log(contenedores);
+
+
+
+// Si una clase no existe, retorna como si fuera un arreglo vacío
+const noExiste = document.getElementsByClassName('no-existe');
+console.log(noExiste);
+
