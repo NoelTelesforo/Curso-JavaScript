c5f71b783782a88c0c77cdda8bdbd21e37f561d0
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 16:52:28 -0600

    Representa cualquier página web cargada en el navegador y sirve como punto de entrada al contenido de la página web

--- includes.js
+++
@@ -1,8 +0,0 @@
-
-// Revisa si un valor existe en un array y devuelve un boolean
-
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
-
-
-const resultado = meses.includes('Enero');
-console.log(resultado); // true
