38673ff3a6192799032bbcfcbacd5bd89eef1461
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 09:01:54 -0600

    Formas de crear funciones

---
+++ funciones.js
@@ -0,0 +1,20 @@
+// Las funciones son una serie de procedimientos o instrucciones, es decir lineas de código van a realizar una acción.
+
+// Declaración de función (Function Declaration)
+
+function sumar() {
+    console.log(2 + 2);
+}
+
+// Se llama la función
+sumar(); // Devuelve 4
+
+
+
+// Expresión de Función (Function Expression)
+
+const suma2 = function() {
+    console.log(3 +3);
+}
+
+suma2(); // Devuelve 6
