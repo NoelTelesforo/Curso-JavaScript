987640e851e0b427c591847ddbe266b4fa738f52
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 09:06:53 -0600

    Diferencias entre funciones y métodos

---
+++ funciones-metodos.js
@@ -0,0 +1,13 @@
+
+// Diferencia entre Métodos y Funciones
+
+const numero1 = 20;
+const numero2 = '20';
+
+
+// Esto es una función, tiene primero el nombre y en el paréntesis se pasa el valor.
+console.log(parseInt(numero2));
+
+
+// Esto es un método, tiene primero el nombre de la variable seguido de un punto y el nombre de un método.
+console.log(numero1.toString())
