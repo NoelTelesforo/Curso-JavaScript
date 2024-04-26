1360ce9c88102c76b90817d906ccfb10773f158a
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 08:01:31 -0600

    Cracion de objetos

--- creacion.js
+++
@@ -1,9 +0,0 @@
-
-
-// El tipo de dato boolean solamente puede tener dos valores: true o false.
-
-const boolean1 = true;
-const boolean2 = false;
-
-console.log(boolean1); //true
-console.log(boolean2); //false
