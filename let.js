fcb90e0048df90017b21ddd2343cb98294dacf00
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-25 20:48:52 -0600

    Variables con la palabra reservada let

---
+++ let.js
@@ -0,0 +1,20 @@
+
+
+// Let, permite inicializar una variable y crear un valor, permiten reasignar el valor
+
+let producto = 'Tablet';
+
+producto = "Monitor";
+
+producto = 30;
+producto = true;
+producto = null;
+
+console.log(producto);
+
+
+
+// Se pueden crear las variables sin asignarle ningun valor:
+
+let precio;
+console.log(precio);
