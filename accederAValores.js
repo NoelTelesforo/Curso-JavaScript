cc558ce3ae613eaa7356596a793b149e432bacf2
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 08:05:46 -0600

    Formas de acceder a propiedades y valores de objetos

---
+++ accederAValores.js
@@ -0,0 +1,23 @@
+
+
+
+// Aceder a los valores de un objeto
+
+const producto = {
+    nombre: 'Laptop',
+    precio: 700,
+    disponible: true
+}
+
+console.log(producto);
+
+
+// Con la sinstaxis de punto podemos acceder a las propiedades de los objetos.
+console.log(producto.precio); // 700
+
+
+// Con [] tambien se puede acceder a las propiedades de los objetos pero no es muy comun:
+
+console.log(producto['precio']); //700
+
+
