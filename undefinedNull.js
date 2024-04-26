94fffbcbf635efd7dc688e7ef9e1aa189300359d
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 07:26:36 -0600

    Comparacion entre undefined y null

---
+++ undefinedNull.js
@@ -0,0 +1,15 @@
+
+
+// Undefined, la variable esta definida pero su valor no, por lo tanto su valor es undefined.
+
+let numero;
+
+console.log(numero); //undefined
+
+
+
+
+// Null, el valor de una variable es null, ECMACS6 dice que los null deben de ser un objeto.
+
+let numero2 = null;
+console.log(numero2);
