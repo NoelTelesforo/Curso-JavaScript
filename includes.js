384b6f865d6e858372a2ca3d580f9cd68fe9b8a3
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 15:42:20 -0600

    Retorna un valor y lo asigna a una variable

---
+++ includes.js
@@ -0,0 +1,8 @@
+
+// Revisa si un valor existe en un array y devuelve un boolean
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+
+const resultado = meses.includes('Enero');
+console.log(resultado); // true
