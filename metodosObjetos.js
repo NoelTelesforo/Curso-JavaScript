70b83b9e24240ce4ab4188e109c4aa89259c0c53
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 08:24:28 -0600

    Métodos que modifican objetos

---
+++ metodosObjetos.js
@@ -0,0 +1,38 @@
+
+
+
+// Métodos para objetos
+
+
+
+
+// Un objeto no puede ser modificado, se habilta el modo estricto, este exige que cumplas ciertas reglas a la hora de escribir código de JavaScript.
+'use struict';
+
+const producto = {
+    nombre : "Monitor 20 Pulgadas",
+    precio: 700,
+    disponible: false
+}
+
+// No permite reasignar propiedades del objeto ni agregar propiedades al objeto:
+
+Object.freeze(producto);
+
+console.log(producto);
+
+
+// Metodo para saber si un objeto esta congelado o no.
+
+console.log(Object.isFrozen(producto));
+
+
+// No se pueden agregar ni eliminar propiedades pero si se pueden modificar las existentes.
+
+Object.seal(producto);
+
+console.log(producto);
+
+
+// Para saber si un objeto esta sellado:
+console.log(Object.isSealed(producto)); // Devuelve un boolean
