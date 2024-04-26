bb32e630a1ce98d1b37d7e005272095d11985c99
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 07:25:30 -0600

    Operadores de comparación

---
+++ operadorComparacion.js
@@ -0,0 +1,27 @@
+
+
+// Comparar dos valores
+
+// Comparador normal, compara los valores
+
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+// Revisar si dos numeros son iguales
+console.log(numero1 == numero3); //false
+console.log(numero1 == numero2); //true
+
+
+
+// Comprador estricto, compara el valor y tipo de dato
+console.log(numero1 === numero2); //false
+
+
+
+// Comparador diferente que...
+const password1 = "admin";
+const password2 = "Admin";
+
+console.log(password1 != password2); //true
+console.log(numero1 != numero2); //false
