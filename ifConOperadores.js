d0fbc0af6d66eba8965672ca29122ebb5e99d37e
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 14:52:33 -0600

    Uso de if con operadores de igualdad y mayor que...

--- ifConOperadores.js
+++ ifConOperadores.js
@@ -30,3 +30,70 @@ if(dinero >= totalAPagar) {
 } else {
     console.log('Fondos insuficientes');
 }
+
+
+// Operador (and) &&
+
+// Revisa que se cumplan dos condiciones dentro del if
+
+const usuario = true;
+const puedePagar = true;
+
+if(usuario && puedePagar) {
+    console.log('Si puedes comprar');
+}  else if (!usuario){
+    console.log('Inicia sesión o crea una cuenta');
+} else if(!puedePagar) {
+    console.log('Fondos insuficientes');
+} else {
+    console.log('No puedes comprar');
+}
+
+
+// Operador Or.
+
+// Revisa que se cumpla una o la otra condición.
+
+// Con una sola condición que se cumpla con eso ejecuta el código.
+
+
+const efectivo = 300;
+const credito = 1000;
+const disponible = efectivo + credito;
+const totalPagar = 600;
+
+
+if(efectivo > totalPagar || credito > totalPagar || disponible > totalPagar) {
+    console.log('Si podemos pagar');
+} else {
+    console.log('Fondos insuficientes');
+}
+
+
+
+// Operador Ternario. (?)
+
+// Si la condición se cumple se ejecuta el código que hay despues del simbolo ? y  si no se cumple se ejecuta el código que hay despues de los dos puntos :
+
+// Forma de comprobar dos condiciones,.
+console.log(usuario || puedePagar ? 'Si puede pagar' : 'No esta autenticado');
+
+
+// Ejemplo de if anidado:
+
+
+if(efectivo > totalPagar || credito > totalPagar || disponible > totalPagar) {
+    if(efectivo > totalPagar) {
+        console.log('pago realizado en efectivo');
+    } else {
+        console.log('Otra forma de pago');
+    }
+} else {
+    console.log('Fondos insuficientes');
+}
+
+
+
+// if anidado dentro de un ternario:
+
+console.log(autenticado ? puedePagar ? 'Si esta autenticado y puede pagar' : 'Si esta autenticado, no puede pagar' : 'No esta autenticado');
