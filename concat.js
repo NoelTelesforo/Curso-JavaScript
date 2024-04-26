8347dda6ea1a1486294d35694e6308c91e5dfdf4
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 16:18:45 -0600

    Une dos o mas arrays

---
+++ concat.js
@@ -0,0 +1,11 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const meses2 = ['Agosto', 'Septiembre'];
+
+const meses3 = ['Octubre'];
+
+
+
+
+const resultado = meses.concat(meses2, meses3);
+console.log(resultado);
