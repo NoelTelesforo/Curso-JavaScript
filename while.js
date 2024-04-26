92e333c4dd5b49acf003041ae26eb33b1a79e7ba
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 15:18:27 -0600

    Se ejecuta mientras la condicion sea verdadera

---
+++ while.js
@@ -0,0 +1,17 @@
+
+
+// Consta de tres partes:
+
+// 1.- Se inicializa.
+// 2.- En el paréntesis se coloca la condición que queremos revisar.
+// 3.- Antes del cierre de la llave se coloca el c¿incremento (i++).
+
+
+let i = 0;
+
+while(i < 10) {
+    console.log(`Número ${i}`);
+
+
+    i++;
+}
