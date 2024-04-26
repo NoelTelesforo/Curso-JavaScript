b4ea66a69a8e02b5f99ec6fe45d858856a3cbaeb
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 17:40:09 -0600

    Propagación de evento

--- eventBubbling.js
+++ eventBubbling.js
@@ -26,3 +26,38 @@ titulo.addEventListener('click', e => {
     e.stopPropagation()
     console.log('Click en titulo');
 });
+
+
+
+
+
+// De esta forma estamos identificando a que le estamos dando click:
+
+cardDiv.addEventListener('click', e => {
+    console.log(e.target);
+});
+
+
+
+
+// Esto nos dice las clases de lo que le estamos dando click
+
+cardDiv.addEventListener('click', e => {
+    console.log(e.target.classList);
+});
+
+
+
+// Delegation para saber a que elemento le dimos click:
+
+cardDiv.addEventListener('click', e => {
+    if(e.target.classList.contains('titulo')) {
+        console.log('Diste click en titulo');
+    }
+    if(e.target.classList.contains('precio')) {
+        console.log('Diste click en precio');
+    }
+    if(e.target.classList.contains('card')) {
+        console.log('Diste click en card');
+    }
+});
