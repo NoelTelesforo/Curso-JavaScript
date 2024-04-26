916287444d61333ff5e078a4222fd0350cb72344
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 07:40:14 -0600

    Comparacion de booleans

---
+++ comparacion.js
@@ -0,0 +1,16 @@
+
+
+// Consejos a la hora de comparar booleans
+
+const autenticado = true;
+
+if(autenticado) {
+    console.log('Sí puedes ver netflix');
+} else {
+    console.log('No, no puedes ver Netflix');
+}
+
+
+// Operador Ternario
+
+console.log( autenticado ? 'Sí esta autenticado' : 'No esta autenticado');
