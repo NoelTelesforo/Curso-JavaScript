04a1c5cc59a5cb5bf2c467fdbb7dc080041d8288
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 06:36:49 -0600

    Jerarquia de las operaciones

---
+++ operacionesJerarquia.js
@@ -0,0 +1,22 @@
+
+
+// Orden en que se ejecutan las operaciones
+
+let resultado;
+
+// primero se multiplica y despues se suma:
+resultado = 20 + 30 * 2;
+
+// Primero se ejecuta lo que hay en el parentesis.
+resultado = (20 + 30) * 2;
+
+
+// Ejemplo claro seria un 20% de descuento en un carrito de compras:
+resultado = (20 + 30 + 40 + 50 + 60) * .2;
+
+
+// Impuesto de 16%
+resultado = (20 + 30) * 1.16;
+
+
+console.log(resultado);
