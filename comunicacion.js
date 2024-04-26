83b662818c3cda0fa360f779c41f52e430336d13
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 09:12:03 -0600

    Formas de comunicación entre las funciones

---
+++ comunicacion.js
@@ -0,0 +1,28 @@
+
+// Una función manda a llamar otra función dentro de ella.
+// Siempre tiene que haber una función que inicie todo.
+
+
+
+iniciarApp();
+
+function iniciarApp() {
+    console.log('Iniciando app...');    //Iniciando app
+
+    segundaFuncion();
+}
+
+function segundaFuncion() {
+    console.log('Desde la segunda función');    // Desde la segunda función
+
+    usuarioAutenticado('Noel');
+}
+
+
+// Autenticand usuario... espere...
+// Usuario autenticado exitosamente: Noel
+
+function usuarioAutenticado(usuario) {
+    console.log('Autenticando usuario... espere...');
+    console.log(`Usuario autenticado exitosamente: ${usuario}`);
+}
