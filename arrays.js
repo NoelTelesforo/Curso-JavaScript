8c2e78f88b4427428297cb0ccfe75ba453dbbc61
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 08:37:49 -0600

    Creación de arrays

--- arrays.js
+++ arrays.js
@@ -1,21 +1,32 @@
 
 
 
-// Cómo acceder a los elementos de un arreglo?
-
-// Si tratas de acceder a una posición que no existe marca undefined.
-
-// La posición es lo que determina como acceder a un arreglo
+// Array, sirve para agrupar elementos.
+// Puede tener multiples elementos que sean iguales.
+// Los arreglos utilizan indices, es decir; cual es la    posicion que tiene ese elemento en el arreglo. Y los arreglos inician en 0.
 
 
+// Formas de crear arrays
 
-const numeros = [10, 20,30, 40, 50, [1, 2 ,3]];
-
-
-// Acceder al arreglo por medio de la posición
-console.log(numeros[2]); //Devuelve 30
+const numeros = [10, 20, 30];
 
 
-// Acceder a un arreglo de arreglos: acedemos a la posición del areglo y despues al sub arreglo:
+const meses = new Array('Enero', 'Febrero', 'Marzo');
 
-console.log(numeros[5][1]); // Devuelve 2.
+
+// Muestra el número de elementos que hay en el arreglo
+console.log(numeros); 
+
+
+
+// Muestra en la conosola el número con la cantidad de elementos en el arreglo, su posición y su valor.
+console.log(meses);
+
+
+
+// Arreglo que contiene datos de todo tipo
+const deTodo = ['Hola', 10, true, 'si', null, {nombre: 'Juan', trabajo: 'Programador'}, [1, 2, 3]];
+
+
+// Muestra el arreglo deTodo con todas sus propiedades, posiciones y valores.
+console.log(deTodo);
