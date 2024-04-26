c5e557bc5bccb1dd6edf9916a7f721819167c5a3
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 08:38:56 -0600

    Formas de acceder a los elementos de un array

---
+++ accesoArrays.js
@@ -0,0 +1,21 @@
+
+
+
+// Cómo acceder a los elementos de un arreglo?
+
+// Si tratas de acceder a una posivción que no existe marca undefined.
+
+// La posición es lo que determina como acceder a un arreglo
+
+
+
+const numeros = [10, 20,30, 40, 50, [1, 2 ,3]];
+
+
+// Acceder al arreglo por medio de la posición
+console.log(numeros[2]); //Devuelve 30
+
+
+// Acceder a un arreglo de arreglos: acedemos a la posición del areglo y despues al sub arreglo:
+
+console.log(numeros[5][1]); // Devuelve 2.
