ed35c3f6f44f88cc7abb7ee6a2b032feb462e247
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-26 06:56:09 -0600

    Opreadores básicos en JavaScript

---
+++ operadoresBasicos.js
@@ -0,0 +1,38 @@
+
+
+// PREGUNTA DE ENTREVISTA QUE SIGNIFICA EL MAS MAS AL INICIO O FINAL...
+
+// Incrementos
+
+let puntaje = 5;
+
+
+// Se manda llamar el puntaje y despues se muestra el mismo resultado inicial
+
+puntaje++; //5
+
+
+// Primero se incrementa y luego se muestra:
+++puntaje; // 6
+
+
+// Decrementos
+
+let puntaje2 = 5;
+
+// Se manda llamar el puntaje y despues se muestra el valor inicial
+puntaje--; //5
+
+
+// Primero se disminuye y luego se muestra:
+--puntaje; // 4
+
+
+// Se le agrega 3
+puntaje += 3;
+
+// Se le resta 3
+puntaje -= 3;
+
+
+console.log(puntaje);
