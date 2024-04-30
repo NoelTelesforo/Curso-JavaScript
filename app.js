0c626e763f48279d70b9bb4516cdaf78c385d7ea
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-30 06:50:17 -0600

    Función que registra todos los event listeners

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+
+
+cargarEventListeners();
+function cargarEventListeners() {
+    
+}
