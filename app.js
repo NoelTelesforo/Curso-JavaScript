3a7f13318642e0ca4aa1ca4af5c3f00942b78e2f
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-04-30 08:02:25 -0600

    Agregar curso

--- app.js
+++ app.js
@@ -6,5 +6,12 @@ const listaCursos = document.querySelector('#lista-cursos');
 
 cargarEventListeners();
 function cargarEventListeners() {
-    
+    listaCursos.addEventListener('clcik', agregarCurso);
+}
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+    }
 }
