b92f3ef0208c93415df05180731a3ab8fd14d8fe
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-02 09:27:05 -0600

    Habilita el boton de enviar

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 // Se ejecuta una vez que todo el contenido de la página se ha cargado.
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
+
+
     // Selecciona los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
 
     // Asignar eventos a los elementos
-    inputEmail.addEventListener('blur', validar);
-    inputAsunto.addEventListener('blur', validar);
-    inputMensaje.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
 
     function validar(e) {
         if (e.target.value.trim() === '') {
             mostrarAlerta(`El campo ${e.target,id} es obligatorio`, e.target.value.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
             return;
         }
 
         if(e.target.id === 'email' && !validarEmail(e.target.value)) {
             mostrarAlerta('Email no válido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
             return;
         }
 
-        limpiarAlerta(e.target.value.parentElement);
+        limpiarAlerta(e.target.parentElement);
+
+        // Asignar valores al objeto email
+        email[e.target.name] = e.target.value.trim().tiLowerCase();
+
+        // Comprueba si el objeto email tiene valores
+        comprobarEmail();
     }
 
     function mostrarAlerta(mensaje, referencia) {
@@ -52,4 +72,16 @@ document.addEventListener('DOMContentLoaded', function() {
         const resultado = regex.test(email);
         return resultado;
     }
+
+    function comprobarEmail() {
+
+        if(Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        }
+
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
+    } 
 });
