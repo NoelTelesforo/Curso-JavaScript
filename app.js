226f8fa37f0de9ee46f8300116a9e5044c43ae07
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-02 08:05:42 -0600

    Selecciona los elementos de la interfaz

---
+++ app.js
@@ -0,0 +1,8 @@
+// Se ejecuta una vez que todo el contenido de la página se ha cargado.
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+});
