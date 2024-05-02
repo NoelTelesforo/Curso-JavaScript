91d418b96a2aa0e4d86c6c95290823e6c5160ebf
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-02 07:17:21 -0600

    Muestra el curso seleccionado en el carrito

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 
 cargarEventListeners();
@@ -27,5 +28,43 @@ function leerDatosCurso(curso) {
         precio: curso.querySelector('.precio span').textContent,
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
+    }
+
+    articulosCarrito = [...articulosCarrito, infoCurso];
+
+    carritoHTML();
+}
+
+
+function carritoHTML() {
+    
+    articulosCarrito.forEach(curso => {
+
+        // Limpia el HTML
+        limpiarHTML();
+
+        const {imagen, titulo, precio, cantidad, id} = curso;
+
+        // Recorre el carrito y genera el HTML
+        const row = documment.createElement('tr');
+        row.innerHTML = `
+            <td> <img src="${imagen}" width="100"> </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}"> X </a>
+            </td>
+        `;
+
+        // Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+    while(contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
     }
 }
