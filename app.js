69c85c8a5f7bb650ebd194b0a51bb550d4ea0e52
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-03 15:34:15 -0600

    Función para agregar tweets

--- app.js
+++ app.js
@@ -4,4 +4,18 @@ const listaTweets = document.querySeslector('#lista-tweets');
 let tweets = [];
 
 
+// Event Listeners
+eventListeners();
 
+
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet);
+}
+
+
+// Funciones
+function agregarTweet(e) {
+    e.preventDefault();
+
+    console.log('Agregando Tweet...');
+}
