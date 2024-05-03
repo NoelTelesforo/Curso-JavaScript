ca17c3bb7aa6c2652a1ef36aef666e030d9b1618
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-03 06:53:55 -0600

    Función que muestra las variables

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Variables
+const resultado = document.querySelector('#resultado');
+
+// Eventos
+docyment.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos();
+});
+
+
+
+// Funciones
+function mostrarAutos() {
+
+    const {marca, modelo, year, puertas, transmision, precio, color }= auto;
+    const autoHMTL = document.createElement('p');
+
+    autoHMTL.textContext = `^
+        ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: ${precio} - Color: ${color}
+    
+    
+    `;
+
+    // Insertar en el HTML
+}
