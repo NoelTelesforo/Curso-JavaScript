cdf63ab6c241777ca7d530ee9c2ce3c249c0f5e1
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-03 17:04:58 -0600

    Objeto Date

---
+++ date.js
@@ -0,0 +1,14 @@
+const diaHoy = new Date();
+
+let valor;
+
+valor = diaHoy;
+
+// Trae el año actual
+valor = diaHoy.getFullYear();
+
+// Trae el mes actual
+valor = diaHoy.getMonth();
+
+// Trae los minutos
+valor = diaHoy.getMinutes();
