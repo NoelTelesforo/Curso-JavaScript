ab6de206a684d854700d651e0257dd905391e9ce
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-03 07:34:22 -0600

    Leé las propiedades del auto

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear();
 const min = max - 10;
+
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -15,6 +34,36 @@ document.addEventListener('DOMContentLoaded', () => {
     llenarSelect();
     
 });
+
+// Event listener para los select de búsqueda
+marca.addEventListener('Change', e => {
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener('Change', e => {
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('Change', e => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('Change', e => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('Change', e => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('Change', e => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('Change', e => {
+    datosBusqueda.color = e.target.value;
+});
+
 
 
 
