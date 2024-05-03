8d1babd5fa3f428f0bdb62003eb0e168c4831bf6
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-03 15:45:39 -0600

    Valida el formulario

--- app.js
+++ app.js
@@ -17,5 +17,32 @@ function eventListeners() {
 function agregarTweet(e) {
     e.preventDefault();
 
+    // Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+
+    // Validación.
+    if(tweet === '') {
+        mostrarError('El mensaje no puede ir vacío');
+        return;
+    }
+
     console.log('Agregando Tweet...');
+}
+
+
+// Mostrar mensaje de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertar mensaje de error en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Elimina la alerta despues de 3 segundos.
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
