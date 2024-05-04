46da520e030fc981983f61358ef37bc6e3379b9b
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-04 08:45:12 -0600

    Modificadores de acceso

---
+++ 03.js
@@ -0,0 +1,20 @@
+// Modificadores de acceso
+
+class Cliente {
+    
+    #nombre;
+
+    setNombre(nombre) {
+        this.#nombre = nombre;
+    }
+
+    getNombre() {
+        return this.#nombre;
+    }
+
+}
+
+
+const juan = new Cliente();
+juan.setNombre('Juan');
+console.log(juan.getNombre());
