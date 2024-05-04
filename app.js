c8bd119bc95d15f8c81439ac7db2ac6d28440b38
Noel Telesforo <ntelesforomartinez@gmail.com>
2024-05-04 07:16:18 -0600

    Calcula el seguro

--- app.js
+++ app.js
@@ -6,6 +6,45 @@ function Seguro(marca, year, tipo) {
     this.tipo = tipo;
 }
 
+// Realiza la cotización con los datos
+Seguro.prototype.cotizarSeguro = function() {
+    let cantidad;
+    const base = 2000;
+
+    switch(this.marca) {
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+        default:
+            break;
+    }
+
+    // Leé el año
+    const diferencia = new Date().getFullYear() - this.year;
+
+    // Por cada año de antigüedad se reduce el 3% el valor del seguro.
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+     /*
+        Si el seguro es básico se multiplica por un 30%
+        Si el seguro es completo se multiplica por un 50%
+    */
+
+    if(this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else {
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
+}
+
 function UI () {}
 
 // Llena las opciones de los años
@@ -79,5 +118,9 @@ function cotizarSeguro(e) {
     }
 
     ui.mostrarMensaje('Cotizando...', 'exito');
+
+    // Instanciar el seguro
+    const seguro = new Seguro(marca, year, tipo);
+    seguro.cotizarSeguro();
     
 }
